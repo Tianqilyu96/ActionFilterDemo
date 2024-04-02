292d3cf6b8f9015d564e9b66e72307850140458a
Tianqi Lyu <Tianqilyu96@gmail.com>
2024-04-03 01:23:22 +1100

    add demo controller

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActionFilterDemo.Controllers;
+[ApiController]
+[Route("[controller]/[action]")]
+public class DemoController(MyDbContext myDbContext) : ControllerBase
+{
+    private readonly MyDbContext? dbContext = myDbContext;
+
+    [HttpPost]
+    public string Test1()
+    {
+        dbContext?.Books.Add(new Book { Title = ".Net Book", Id = 0 });
+        return "Success Added";
+    }
+}
